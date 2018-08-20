99f219870688bad350f47f9c87ae1acac3de13fb
Ricky Wong <rickywong@gmail.com>
2018-08-20 15:44:34 -0700

    Add logic for setting active room class

--- app/components/views/RoomLink.js
+++ app/components/views/RoomLink.js
@@ -1,6 +1,7 @@
 import Marionette from 'backbone.marionette';
 import RoomLinkTemplate from './RoomLink.jst';
 import Radio from 'backbone.radio';
+import $ from 'jquery';
 
 const channel = Radio.channel('chatApp');
 
@@ -8,11 +9,17 @@ export default Marionette.View.extend({
   template: RoomLinkTemplate,
 
   events: {
-    'click .room': 'loadRoom'
+    'click @ui.roomLink': 'loadRoom'
+  },
+
+  ui: {
+    roomLink: '.room-link'
   },
 
   loadRoom(e) {
     e.preventDefault();
+    this.ui.roomLink.closest('#rooms').find('.room-link').removeClass('active');
+    this.ui.roomLink.addClass('active');
     channel.request('loadRoom', this.model.id);
   }
 });
